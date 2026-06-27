e0eb84bc0207223d45ed7eddd44cf5fffa9db66c
smiley dinesh <smileydinesheee@gmail.com>
2026-06-27 12:53:55 +0530

    Your changes

--- config/urls.py
+++ config/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mobiles.urls')),
+    path('beauty/', include('beauty.urls')),
 ]
 
 if settings.DEBUG:
