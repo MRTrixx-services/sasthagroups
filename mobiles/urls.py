f4b2b0df7ea1ed5a3bc59b046330f56d9d3a74e9
smiley dinesh <smileydinesheee@gmail.com>
2026-07-02 03:01:32 +0530

    flowers and mobiles page addede

--- mobiles/urls.py
+++ mobiles/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import home,mobiles
 
 urlpatterns = [
     path('', home, name='home'),
+    path("mobiles/", mobiles, name="mobiles"),
+
 ]
--- mobiles/views.py
+++ mobiles/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'mobiles/home.html')
+
+def mobiles(request):
+    return render(request, "mobileshop/mobiles.html")
